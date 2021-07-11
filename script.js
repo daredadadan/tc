881bc23adb9a3f98fea35d2f21f8382ee82f1b9d
akurakuu <77902040+akurakuu@users.noreply.github.com>
2021-07-11 17:22:01 +0900

    update

--- script.js
+++ script.js
@@ -13,12 +13,12 @@ function buttonClick() {
     switch (switchButton) {
         case false:
             nav.classList.add('navAni');
-            menuButton_icon.innerHTML = '<object id="menuButton_svg" data="style/menu_close.svg" width="32px"></object>';
+            //menuButton_icon.innerHTML = '<object id="menuButton_svg" data="style/menu_close.svg" width="32px"></object>';
             switchButton = true;
             break;
         case true:
             nav.classList.remove('navAni');
-            menuButton_icon.innerHTML = '<object id="menuButton_svg" data="style/menu_button.svg" width="32px"></object>';
+            //menuButton_icon.innerHTML = '<object id="menuButton_svg" data="style/menu_button.svg" width="32px"></object>';
             switchButton = false;
             break;
     }
