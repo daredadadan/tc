0636cb8d0e6d806daa480262e592a15d0b92216e
akurakuu <77902040+akurakuu@users.noreply.github.com>
2021-07-11 17:07:06 +0900

    change homepage

---
+++ script.js
@@ -0,0 +1,26 @@
+//var menuButton = document.getElementsByClassName('menuButton');
+window.onload = function () {
+    var nav = document.getElementById('nav').innerHTML;
+    var local_links = document.getElementById('local_links');
+    local_links.innerHTML = nav;
+}
+
+var switchButton = false;
+function buttonClick() {
+    var nav = document.getElementById('nav');
+    var menuButton_icon = document.getElementById('menuButton');
+    //var menuButton = document.getElementById('menuButton');
+    switch (switchButton) {
+        case false:
+            nav.classList.add('navAni');
+            menuButton_icon.innerHTML = '<object id="menuButton_svg" data="style/menu_close.svg" width="32px"></object>';
+            switchButton = true;
+            break;
+        case true:
+            nav.classList.remove('navAni');
+            menuButton_icon.innerHTML = '<object id="menuButton_svg" data="style/menu_button.svg" width="32px"></object>';
+            switchButton = false;
+            break;
+    }
+
+}
